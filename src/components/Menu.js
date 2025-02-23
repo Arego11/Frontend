c96047fc8ce6005c5a93eeee63c20d196cd865c1
Areg Hovumyan <ahovumy363@student.glendale.edu>
2025-02-23 00:49:08 -0800

    menu hamburger button changed to more family friendly :)

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,23 +8,33 @@ const Menu = () => {
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
-    document.querySelector('.main-content').classList.toggle('blurred', !isOpen);
+    document.querySelector('.main-content')
+        .classList.toggle('blurred', !isOpen);
   };
 
   return (
-    <div className="menu-container">
-      <button className="menu-button" onClick={toggleMenu}>☰</button>
+      <div className = "menu-container">
+      <label htmlFor = "check" className = "menu-button">
+      <input type = "checkbox" id = "check" checked = {isOpen} onChange =
+       { toggleMenu } />
+        <span></span><span>
+      </span>
+        <span></span>
+      </label>
       <div className={`menu-tab ${isOpen ? 'open' : ''}`}>
         <ul>
-          <li><a href="#option1">Profile</a></li>
-          <li><a href="#option2">Subscription</a></li>
-          <li><a href="#option3">Chat</a></li>
-          <li><a href="#option4">Collection</a></li>
+          <li><a href="#option1">Profile</a>
+      </li>
+          <li><a href="#option2">Subscription</a>
+      </li>
+          <li><a href="#option3">Chat</a>
+      </li>
+          <li><a href="#option4">Collection</a>
+      </li>
           <li><a href="#option5">Generate</a></li>
         </ul>
       </div>
-    </div>
-  );
+    </div>);
 };
 
 export default Menu;
